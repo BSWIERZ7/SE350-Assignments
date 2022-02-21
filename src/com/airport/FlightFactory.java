5348d3943e8614e0f7201465f47d6d696355612a
BSWIERZ7 <bart601722@gmail.com>
2022-02-20 18:56:03 -0600

    flightfactory

--- src/com/airport/FlightFactory.java
+++ src/com/airport/FlightFactory.java
@@ -32,7 +32,34 @@ public class FlightFactory {
 
         else return null;
     }
+    //test
+    //airport and airline is the same for all flights so recycle that section. Part that changes is flightnumber and passengercapacity
+    //add a getFlight()
+    //public static Flight getFlight(String flight) {return flight};
 
-    //Create hashmap - create one of each flight - in our case two types, a commercial and
+    /* //before changes w/ flyweight pattern
+    public static Flight createFlight(String type, Airline airline, Airport origin, Airport destination, int passengerCapacity) {
+        if (type.equals("commercialFlight")) { //create commercialFlight
+            try {
+                return new CommercialFlight(airline, origin, destination);
+            } catch(NullParameterException e) {
+                return null;
+            }
+        }
+        else if (type.equals("passengerFlight")) { //create passangerFlight
+            try {
+                return new PassengerFlight(airline, origin, destination, passengerCapacity);
+            } catch(NullParameterException e) {
+                return null;
+            } catch(BadParameterException e) {
+                return null;
+            }
+        }
+        else return null;
+    }
+    */
+
+    //add flyweight pattern within the factory - each time we request an object, the factory checks if it has been created
+    //Create hashmap - create one of each flight - in our case two types, a commercial and passenger
 }
 
